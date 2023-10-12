4788b6a085970153941ebba2c4474efb757e4537
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2023-10-12 21:04:30 +0200

    Add CameraTarget for mouse movement

---
+++ Assets/CameraTarget.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTarget : MonoBehaviour {
+    [SerializeField] private new Camera camera;
+    [SerializeField] private Transform player;
+    [SerializeField] private float threshold;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    private void Update() {
+        var mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
+        var position = player.position;
+        var targetPos = (position + mousePosition) / 2f;
+
+        targetPos.x = Mathf.Clamp(targetPos.x, -threshold + position.x, threshold + position.x);
+        targetPos.y = Mathf.Clamp(targetPos.y, -threshold + position.y, threshold + position.y);
+
+        transform.position = targetPos;
+    }
+}

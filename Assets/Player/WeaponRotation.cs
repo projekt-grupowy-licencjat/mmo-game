3d40ecadefc142d2fc3d97b70a0d76fc39cfb1b6
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2023-10-28 17:28:08 +0200

    Modify weapon sprite pivot + Gungeon-esque weapon rotation W.I.P

--- Assets/Player/WeaponRotation.cs
+++ Assets/Player/WeaponRotation.cs
@@ -1,27 +1,37 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponRotation : MonoBehaviour {
     [SerializeField] private SpriteRenderer spriteRenderer;
     [SerializeField] private new Camera camera;
     private Transform _localTransform;
+    [SerializeField] private float maxThreshold;
 
     private void Start() {
         _localTransform = transform;
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         camera = Camera.main;
     }
 
     private void Update() {
         Vector2 mouseScreenPosition = camera.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 position = _localTransform.position;
+        Vector2 position = _localTransform.parent.gameObject.transform.position;
         
         // Rotate weapon
-        _localTransform.right =(mouseScreenPosition - position).normalized;
+        _localTransform.right =(mouseScreenPosition - (Vector2)transform.position).normalized;
+        
+        // TODO Rotate around player - W.I.P
+        var allowedPos = mouseScreenPosition - position;
+        allowedPos = Vector3.ClampMagnitude(allowedPos, maxThreshold);
+        transform.position = position + allowedPos;
 
         // Flip weapon sprite
         var mousePosToPlayer = mouseScreenPosition - position;
         spriteRenderer.flipY = mousePosToPlayer.x < 0;
+        
+        // TODO Switch hands - W.I.P
+        var transformLocalPosition = _localTransform.position;
+        if (mousePosToPlayer.x < 0) transformLocalPosition.x = position.x - maxThreshold;
+        else transformLocalPosition.x = position.x + maxThreshold;
+        transform.position = transformLocalPosition;
     }
 }

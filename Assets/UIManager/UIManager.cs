c11461996cdf1c668bfb94fc2b1a175b5a984fc5
piotrselak <piotr.selak.it@gmail.com>
2023-11-01 12:15:04 +0100

    Further progress on ui manager

--- Assets/UIManager/UIManager.cs
+++ Assets/UIManager/UIManager.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace UIManager
@@ -6,23 +8,39 @@ namespace UIManager
     public class UIManager : MonoBehaviour
     {
         [SerializeField] private List<KeycodeElement> keycodeElements;
-        private List<GameObject> _activeElements;
+        [SerializeField] private List<GameObject> staticElements;
+        
+        private List<Tuple<int, GameObject>> _activeElements;
         
         private void Start()
         {
-            _activeElements = new List<GameObject>();
+            _activeElements = new List<Tuple<int, GameObject>>();
+
+            foreach (var element in staticElements)
+            {
+                // TODO: spawn in children canvas here
+            }
         }
 
         private void Update()
         {
-            foreach (var element in keycodeElements)
+            foreach (var (element, index) in keycodeElements.Select((item, index) => (item, index)))
             {
                 if (Input.GetKeyDown(element.Keycode) && !element.Active)
                 {
                     element.Active = true;
+                    // TODO: Refactor it to be instantiated in canvas of this manager
                     var newElement = Instantiate(element.Element);
-                    _activeElements.Add(newElement);
+                    _activeElements.Insert(0, Tuple.Create(index, newElement));
                 }
+            }
+
+            if (Input.GetKeyDown(KeyCode.Escape) && _activeElements.Count != 0)
+            {
+                int keycodeIndex = _activeElements[0].Item1;
+                keycodeElements[keycodeIndex].Active = false;
+                Destroy(_activeElements[0].Item2);
+                _activeElements.RemoveAt(0);
             }
         }
     }

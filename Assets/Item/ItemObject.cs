52c965f132f30177f5e48905f45af2e5e35286a2
piotrselak <piotr.selak.it@gmail.com>
2023-11-06 17:29:50 +0100

    wip

---
+++ Assets/Item/ItemObject.cs
@@ -0,0 +1,21 @@
+using System;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Item
+{
+    // This class represents an item in case of laying on the map to be picked
+    // This is the only component needed to be added for a GameObject to be considered 
+    // an item - which will ease creation of ItemObjects in runtime.
+    public class ItemObject : NetworkBehaviour
+    {
+        public ItemData data;
+        private SpriteRenderer _spriteRenderer;
+
+        private void Start()
+        {
+            _spriteRenderer = gameObject.AddComponent<SpriteRenderer>();
+            _spriteRenderer.sprite = data.itemSprite;
+        }
+    }
+}

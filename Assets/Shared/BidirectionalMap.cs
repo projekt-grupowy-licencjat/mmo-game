bdcfebc3dc9d56efb0eb0ce95711ce9b675ded23
piotrselak <piotr.selak.it@gmail.com>
2023-11-24 22:02:17 +0100

    Added Bidirectional map implementation

---
+++ Assets/Shared/BidirectionalMap.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace Shared
+{
+    public class BidirectionalMap<TKey, TValue>
+    {
+        private Dictionary<TKey, TValue> forward = new Dictionary<TKey, TValue>();
+        private Dictionary<TValue, TKey> reverse = new Dictionary<TValue, TKey>();
+
+        public void Add(TKey key, TValue value)
+        {
+            forward.Add(key, value);
+            reverse.Add(value, key);
+        }
+
+        public bool RemoveByFirst(TKey key)
+        {
+            TValue second;
+            if (forward.TryGetValue(key, out second))
+            {
+                forward.Remove(key);
+                reverse.Remove(second);
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool RemoveBySecond(TValue value)
+        {
+            TKey first;
+            if (reverse.TryGetValue(value, out first))
+            {
+                reverse.Remove(value);
+                forward.Remove(first);
+                return true;
+            }
+
+            return false;
+        }
+        
+        public bool TryGetByFirst(TKey key, out TValue value)
+        {
+            return forward.TryGetValue(key, out value);
+        }
+
+        public bool TryGetBySecond(TValue value, out TKey key)
+        {
+            return reverse.TryGetValue(value, out key);
+        }
+    }
+}

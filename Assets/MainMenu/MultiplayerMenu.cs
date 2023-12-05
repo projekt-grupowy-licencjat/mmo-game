3c9c7462faba66c90a22f18d567b241baae36363
piotrselak <piotr.selak.it@gmail.com>
2023-12-06 00:55:37 +0100

    Wip

--- Assets/MainMenu/MultiplayerMenu.cs
+++ Assets/MainMenu/MultiplayerMenu.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class MultiplayerMenu : MonoBehaviour
 {
     public TMP_InputField input;
-
+    
     public void SubmitInput()
     { 
         var inputValue = input.text;
@@ -23,6 +25,8 @@ public class MultiplayerMenu : MonoBehaviour
     public void HostServer()
     {
             Debug.Log("hosting");
+            NetworkManager.Singleton.StartHost();
+            SceneManager.LoadScene("Hub");
     }
 
 }
